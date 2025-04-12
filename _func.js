cf46c070e9bf35bcb4f4d7805dce9ff8fca383c8
Ahmad Beni Rusli <159234974+ahmdbnrsl@users.noreply.github.com>
2025-04-13 01:03:18 +0700

    Update _func.js

--- _func.js
+++ _func.js
@@ -1,10 +1,10 @@
-import { writeFileSync } from "node:fs";
+
 
 function _write_file(str) {
-    writeFileSync("untukmu.txt", Buffer.from(str, "utf-8"));
+    Deno.writeTextFileSync("untukmu.txt", str);
 }
 function _buff(str) {
-    return Buffer.from(str, "base64").toString("utf-8");
+    return atob(str);
 }
 function _chr(arr) {
     return String.fromCharCode(...arr);
